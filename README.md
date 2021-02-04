7de931f2c29b20bc051841af73ada649f7019b8b
algins <igors.algins@gmail.com>
2021-02-04 20:59:17 +0200

    add json diff generator

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+# php-project-lvl2
+
+[![Actions Status](https://github.com/algins/php-project-lvl2/workflows/CI/badge.svg)](https://github.com/algins/php-project-lvl2/actions)
+[![Maintainability](https://api.codeclimate.com/v1/badges/37847b5a9ca6d9b2d90a/maintainability)](https://codeclimate.com/github/algins/php-project-lvl2/maintainability)
+
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/algins/php-project-lvl2/workflows/hexlet-check/badge.svg)](https://github.com/algins/php-project-lvl2/actions)
+
+### Installation:
+(https://asciinema.org/a/uh5hbImfCz5cwC4J87HBG0S79)
+
+### Usage:
+(https://asciinema.org/a/uTb0DXBvf73R9mLS4RMEpIWa6)
---
+++ src/Differ.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Differ\Differ;
+
+use function Funct\Collection\union;
+
+function genDiff(string $path1, string $path2): string
+{
+    $data1 = readFile($path1);
+    $data2 = readFile($path2);
+
+    $arr1 = json_decode($data1, true);
+    $arr2 = json_decode($data2, true);
+
+    $keys1 = array_keys($arr1);
+    $keys2 = array_keys($arr2);
+
+    $keys = union($keys1, $keys2);
+    sort($keys);
+
+    $diff =  array_reduce($keys, function ($acc, $key) use ($arr1, $arr2) {
+        $previousValue = is_bool($arr1[$key]) ? $arr1[$key] ? 'true' : 'false' : $arr1[$key];
+        $currentValue = is_bool($arr2[$key]) ? $arr2[$key] ? 'true' : 'false' : $arr2[$key];
+
+        if (!array_key_exists($key, $arr1)) {
+            $acc[] = "  + {$key}: {$currentValue}";
+            return $acc;
+        }
+
+        if (!array_key_exists($key, $arr2)) {
+            $acc[] = "  - {$key}: {$previousValue}";
+            return $acc;
+        }
+
+        if ($arr1[$key] !== $arr2[$key]) {
+            $acc[] = "  - {$key}: {$previousValue}";
+            $acc[] = "  + {$key}: {$currentValue}";
+            return $acc;
+        }
+
+        $acc[] = "    {$key}: {$currentValue}";
+
+        return $acc;
+    }, []);
+
+    return "{\n" . implode("\n", $diff) . "\n}\n";
+}
+
+function readFile(string $path): string
+{
+    $realPath = realpath($path);
+    $data = file_get_contents($realPath);
+
+    return $data;
+}
