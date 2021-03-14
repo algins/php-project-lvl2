22b5fda00dede00f0fc5176e6f1b40232787bf41
algins <igors.algins@gmail.com>
2021-03-14 10:31:35 +0200

    refactor Stylish formatter

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -14,31 +14,19 @@ function buildLines(array $diff): array
     $mapping = [
         'added' => function (string $key, array $values): array {
             ['current' => $current] = $values;
-            return [
-                "  + {$key}: {$current}",
-            ];
+            return ["  + {$key}: {$current}"];
         },
         'removed' => function (string $key, array $values): array {
             ['previous' => $previous] = $values;
-            return [
-                "  - {$key}: {$previous}",
-            ];
+            return ["  - {$key}: {$previous}"];
         },
         'changed' => function (string $key, array $values): array {
-            [
-                'current' => $current,
-                'previous' => $previous,
-            ] = $values;
-            return [
-                "  - {$key}: {$previous}",
-                "  + {$key}: {$current}",
-            ];
+            ['current' => $current, 'previous' => $previous] = $values;
+            return ["  - {$key}: {$previous}", "  + {$key}: {$current}"];
         },
         'unchanged' => function (string $key, array $values): array {
             ['current' => $current] = $values;
-            return [
-                "    {$key}: {$current}",
-            ];
+            return ["    {$key}: {$current}"];
         },
     ];
 
@@ -54,21 +42,22 @@ function buildFromMapping(array $diff, array $mapping): array
             'values' => $values,
         ] = $item;
 
-        $formattedValues = array_map(function ($value): string {
-            return stringify($value);
-        }, $values);
+        $stringifiedValues = stringifyValues($values);
 
-        return array_merge($acc, $mapping[$state]($key, $formattedValues));
+        return array_merge($acc, $mapping[$state]($key, $stringifiedValues));
     }, []);
 }
 
-function stringify($value): string
+function stringifyValues(array $values): array
 {
-    if (is_bool($value)) {
-        return $value ? 'true' : 'false';
-    };
+    return array_map(function ($value): string {
+        return is_bool($value) ? stringifyBool($value) : (string) $value;
+    }, $values);
+}
 
-    return (string) $value;
+function stringifyBool(bool $value): string
+{
+    return $value ? 'true' : 'false';
 }
 
 function render(array $lines): string
