d46fa017a059d88cde51c5e5bf1967f8393f68d5
algins <igors.algins@gmail.com>
2021-05-24 22:17:15 +0300

    fix json formatter final newline

--- src/Formatters/Json.php
+++ src/Formatters/Json.php
@@ -4,5 +4,5 @@ namespace Differ\Formatters\Json;
 
 function format(array $diff): string
 {
-    return json_encode($diff, JSON_PRETTY_PRINT);
+    return json_encode($diff, JSON_PRETTY_PRINT) . "\n";
 }
