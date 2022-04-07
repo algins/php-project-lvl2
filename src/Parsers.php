727e22e32652f2ae06c50811c0ffdf76c5417cb3
algins <igors.algins@gmail.com>
2022-04-07 18:31:26 +0300

    Refactor parse

--- src/Parsers.php
+++ src/Parsers.php
@@ -9,16 +9,14 @@ const TYPE_YAML = 'yaml';
 
 function parse(string $data, string $type): object
 {
-    $parsers = [
-        TYPE_JSON => function (string $data): object {
+    switch ($type) {
+        case TYPE_JSON:
             return parseJson($data);
-        },
-        TYPE_YAML => function (string $data): object {
+        case TYPE_YAML:
             return parseYaml($data);
-        }
-    ];
-
-    return $parsers[$type]($data);
+        default:
+            throw new Exception('Invalid type!');
+    }
 }
 
 function parseJson(string $data): object
